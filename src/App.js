2b5aae967dc1d7f3473bfad72cf65c1d24fb0001
annema01 <123336005+annema01@users.noreply.github.com>
2023-07-26 17:34:03 -0400

    Weather component created

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import "./App.css"
+import Weather from "./Weather"
 
 function App() {
   return (
     <div className='App'>
+      <Weather />
       <h1>
         Hello <i class='bi bi-cone-striped'></i>
       </h1>
---
+++ src/Weather.js
@@ -0,0 +1,5 @@
+import "./Weather.css"
+
+export default function Weather() {
+  return <div>Hello from Weather</div>
+}
