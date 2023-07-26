b20a89032012b601c59dd30427b3963682e6c60e
annema01 <123336005+annema01@users.noreply.github.com>
2023-07-26 17:25:29 -0400

    bootstrap + bootstrap icons installed

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
 import "./App.css"
 
 function App() {
-  return <div className='App'>Test</div>
+  return (
+    <div className='App'>
+      <h1>
+        Hello <i class='bi bi-cone-striped'></i>
+      </h1>
+    </div>
+  )
 }
 
 export default App
--- src/index.js
+++ src/index.js
@@ -1,5 +1,9 @@
 import React from "react"
 import ReactDOM from "react-dom/client"
+import "bootstrap/dist/css/bootstrap.css"
+// Put any other imports below so that CSS from your
+// components takes precedence over default styles.
+import "bootstrap-icons/font/bootstrap-icons.css"
 import "./index.css"
 import App from "./App"
 
