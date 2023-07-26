e67333519fa5fb10dc0ec1a6431e4843f469315d
annema01 <123336005+annema01@users.noreply.github.com>
2023-07-26 17:35:52 -0400

    header form html added to weather

--- src/Weather.js
+++ src/Weather.js
@@ -1,5 +1,16 @@
 import "./Weather.css"
 
 export default function Weather() {
-  return <div>Hello from Weather</div>
+  return (
+    <div className='container-fluid'>
+      Weather App
+      <header>
+        <div>
+          <i className='bi bi-search'></i>
+        </div>
+        <input type='search' />
+        <input type='submit'></input>
+      </header>
+    </div>
+  )
 }
