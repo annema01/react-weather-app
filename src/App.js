d30a0b3c6a6bc4071eefa96f85d3ae7188949630
annema01 <123336005+annema01@users.noreply.github.com>
2023-07-26 22:06:02 -0400

    footer content moved to footer component

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import "./App.css"
 import Weather from "./Weather"
 import Infos from "./Infos"
 import Forcast from "./Forcast"
+import Footer from "./Footer"
 
 function App() {
   return (
@@ -34,47 +35,7 @@ function App() {
         target='#5'
       />
 
-      <footer>
-        © 2023 -{" "}
-        <a
-          href='https://github.com/annema01/react-weather-app'
-          target='_blank'
-          rel='noreferrer'
-          alt='github.com/annema01/react-weather-app'
-        >
-          Coded
-        </a>{" "}
-        by{" "}
-        <a
-          href='https://github.com/annema01'
-          target='_blank'
-          rel='noreferrer'
-          alt='github.com/annema01'
-        >
-          Anne-Marie Robert
-        </a>{" "}
-        with{" "}
-        <a
-          href='https://www.shecodes.io/'
-          target='_blank'
-          rel='noreferrer'
-          alt='shecodes.io'
-        >
-          SheCodes React
-        </a>
-        <div>
-          {" "}
-          Images by{" "}
-          <a
-            href='https://www.freepik.com/free-vector/hand-drawn-weather-effects_18895324.htm#query=weather&position=0&from_view=search&track=sph'
-            target='_blank'
-            rel='noreferrer'
-            alt='freepik.com'
-          >
-            Freepik
-          </a>
-        </div>
-      </footer>
+      <Footer />
     </div>
   )
 }
--- src/Footer.js
+++ src/Footer.js
@@ -2,5 +2,49 @@ import React from "react"
 import "./Footer.css"
 
 export default function Footer() {
-  return <div className='Footer'></div>
+  return (
+    <div className='Footer'>
+      <footer>
+        © 2023 -{" "}
+        <a
+          href='https://github.com/annema01/react-weather-app'
+          target='_blank'
+          rel='noreferrer'
+          alt='github.com/annema01/react-weather-app'
+        >
+          Coded
+        </a>{" "}
+        by{" "}
+        <a
+          href='https://github.com/annema01'
+          target='_blank'
+          rel='noreferrer'
+          alt='github.com/annema01'
+        >
+          Anne-Marie Robert
+        </a>{" "}
+        with{" "}
+        <a
+          href='https://www.shecodes.io/'
+          target='_blank'
+          rel='noreferrer'
+          alt='shecodes.io'
+        >
+          SheCodes React
+        </a>
+        <div>
+          {" "}
+          Images by{" "}
+          <a
+            href='https://www.freepik.com/free-vector/hand-drawn-weather-effects_18895324.htm#query=weather&position=0&from_view=search&track=sph'
+            target='_blank'
+            rel='noreferrer'
+            alt='freepik.com'
+          >
+            Freepik
+          </a>
+        </div>
+      </footer>
+    </div>
+  )
 }
