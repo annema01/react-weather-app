787d9d1956db12df41b815d5af9ed6a0db9abe2b
annema01 <123336005+annema01@users.noreply.github.com>
2023-07-31 13:37:20 -0400

    infos section style

--- src/Infos.js
+++ src/Infos.js
@@ -1,7 +1,5 @@
 import React from "react"
 import "./Infos.css"
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
-import { icon } from "@fortawesome/fontawesome-svg-core/import.macro"
 
 export default function Infos() {
   return (
@@ -12,14 +10,14 @@ export default function Infos() {
       >
         <div className='row'>
           <div className='col-6'>
-            <i className='bi bi-cloud-haze2'></i>
+            <i className='icon bi bi-cloud-haze2'></i>
             <div className='title'>Air quality</div>
             <div className='data text'>
               Very poor <span className='unit'></span>
             </div>
           </div>
           <div className='col-6'>
-            <i className='bi bi-droplet'></i>
+            <i className='icon bi bi-droplet'></i>
             <div className='title'>Humidity</div>
             <div className='data integer'>
               25 <span className='unit'>%</span>
@@ -28,17 +26,23 @@ export default function Infos() {
         </div>
         <div className='row'>
           <div className='col-6'>
-            <i className='bi bi-wind'></i>
+            <i className='icon bi bi-wind'></i>
             <div className='title'>Wind speed</div>
             <div className='data integer'>
-              <FontAwesomeIcon
-                icon={icon({ name: "location-arrow", style: "solid" })}
-              />
+              <div>
+                <svg
+                  className='windDirection'
+                  xmlns='http://www.w3.org/2000/svg'
+                  viewBox='0 0 448 512'
+                >
+                  <path d='M429.6 92.1c4.9-11.9 2.1-25.6-7-34.7s-22.8-11.9-34.7-7l-352 144c-14.2 5.8-22.2 20.8-19.3 35.8s16.1 25.8 31.4 25.8H224V432c0 15.3 10.8 28.4 25.8 31.4s30-5.1 35.8-19.3l144-352z' />
+                </svg>
+              </div>
               6.7 <span className='unit'>m/s</span>
             </div>
           </div>
           <div className='col-6'>
-            <i className='bi bi-tornado'></i>
+            <i className='icon bi bi-tornado'></i>
             <div className='title'>Wind gust</div>
             <div className='data integer'>
               11.6 <span className='unit'>m/s</span>
@@ -47,17 +51,15 @@ export default function Infos() {
         </div>
         <div className='row'>
           <div className='col-6'>
-            <i className='bi bi-umbrella'></i>
+            <i className='icon bi bi-umbrella'></i>
             <div className='title'>Rain</div>
             <div className='data integer'>
               1.5 <span className='unit'>mm</span>
             </div>
           </div>
           <div className='col-6'>
-            <div>
-              <i className='bi bi-arrow-up-short d-block'></i>
-              <i className='bi bi-arrow-down-short d-block'></i>
-            </div>
+            <i className='icon bi bi-arrow-down-up'></i>
+
             <div className='title'>Pressure</div>
             <div className='data integer'>
               1050 <span className='unit'>hPa</span>
@@ -66,14 +68,14 @@ export default function Infos() {
         </div>
         <div className='row'>
           <div className='col-6'>
-            <i className='bi bi-eye'></i>
+            <i className='icon bi bi-eye'></i>
             <div className='title'>Visibility</div>
             <div className='data integer'>
               11 <span className='unit'>km</span>
             </div>
           </div>
           <div className='col-6'>
-            <i className='bi bi-clouds'></i>
+            <i className='icon bi bi-clouds'></i>
             <div className='title'>Overcast clouds</div>
             <div className='data integer'>
               75 <span className='unit'>%</span>
@@ -82,14 +84,14 @@ export default function Infos() {
         </div>
         <div className='row'>
           <div className='col-6'>
-            <i className='bi bi-sunrise'></i>
+            <i className='icon bi bi-sunrise'></i>
             <div className='title'>Sunrise</div>
             <div className='data text'>
               05:25 <span className='unit'></span>
             </div>
           </div>
           <div className='col-6'>
-            <i className='bi bi-sunset'></i>
+            <i className='icon bi bi-sunset'></i>
             <div className='title'>Sunset</div>
             <div className='data text'>
               20:34 <span className='unit'></span>
