5d897a1870d37815bcccf14328b499b02bcef250
annema01 <123336005+annema01@users.noreply.github.com>
2023-08-22 13:05:01 -0400

    fixed the ready state

--- src/Weather.js
+++ src/Weather.js
@@ -1,20 +1,15 @@
 import React, { useState } from "react";
-import Infos from "./Infos"
+import Infos from "./Infos";
 
-import FormatedDate from "./FormatedDate"
-import "./Weather.css"
-import axios from "axios"
-import wind from "./backgroundImages/wind.svg"
+import FormatedDate from "./FormatedDate";
+import "./Weather.css";
+import axios from "axios";
+import wind from "./backgroundImages/wind.svg";
 import FormatedTimezone from "./FormatedTimezone";
 
-
-
-
-
 export default function Weather(props) {
-
-  const [ weatherData, setWeatherData ] = useState(null);
-  const [ city, setCity ] = useState(props.defaultCity);
+  const [weatherData, setWeatherData] = useState({ ready: false });
+  const [city, setCity] = useState(props.defaultCity);
 
   function handleResponse(response) {
     setWeatherData({
@@ -26,7 +21,7 @@ export default function Weather(props) {
       max: response.data.main.temp_max,
       city: response.data.name,
       country: response.data.sys.country,
-      description: response.data.weather[ 0 ].description,
+      description: response.data.weather[0].description,
       date: new Date(response.data.dt * 1000),
       timezone: response.data.timezone,
       //infos
@@ -57,108 +52,101 @@ export default function Weather(props) {
   function search() {
     const apiKey = "f3009e4852fa0a079dab291dabf020c4";
     let unit = "metric";
-    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=${unit}`
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=${unit}`;
     axios.get(apiUrl).then(handleResponse);
   }
 
   if (weatherData.ready) {
     return (
-      <div className='Weather'>
-
-        <div className='container'>
+      <div className="Weather">
+        <div className="container">
           <header id="scrollUp">
-            <form onSubmit={ handleSubmit } className="col-md-5">
+            <form onSubmit={handleSubmit} className="col-md-5">
               <input
-                type='search'
-                placeholder='Enter a city...'
-                autoFocus='on'
-                onChange={ handleCityChange }
+                type="search"
+                placeholder="Enter a city..."
+                autoFocus="on"
+                onChange={handleCityChange}
               />
-              <button
-                className='btn '
-                type='submit'
-              >
-                <i className='bi bi-search'></i>
+              <button className="btn " type="submit">
+                <i className="bi bi-search"></i>
               </button>
             </form>
             <div className="unitButtons col">
-              <a
-                href='/'
-                className='celsius active'
-              >
+              <a href="/" className="celsius active">
                 °C
               </a>
-              <a
-                href='/'
-                className='fahrenheit '
-              >
+              <a href="/" className="fahrenheit ">
                 °F
               </a>
             </div>
           </header>
 
           <div className="content">
-            <div className='updateTime'>Updated<span className="formatedTime"><FormatedDate date={ weatherData.date } /></span></div>
-            {/* Buttons top */ }
+            <div className="updateTime">
+              Updated
+              <span className="formatedTime">
+                <FormatedDate date={weatherData.date} />
+              </span>
+            </div>
+            {/* Buttons top */}
 
-            <a
-              href='#forcast'
-              className='btn forcastAnchor'
-            >
+            <a href="#forcast" className="btn forcastAnchor">
               5 days
-              <i className='bi bi-caret-down'></i>
+              <i className="bi bi-caret-down"></i>
             </a>
-            <a
-              href='/'
-              className='celsiusSm active'
-            >
+            <a href="/" className="celsiusSm active">
               °C
             </a>
-            <a
-              href='/'
-              className='fahrenheitSm '
-            >
+            <a href="/" className="fahrenheitSm ">
               °F
             </a>
             <div>
               <div
-                className='currentWeather'
-                style={ {
+                className="currentWeather"
+                style={{
                   backgroundImage: `url(${wind})`,
                   backgroundSize: "75%",
-                  backgroundRepeat: 'no-repeat',
+                  backgroundRepeat: "no-repeat",
                   backgroundPosition: "right 10px top 25px",
-                } }>
-                <div className='degree'>{ Math.round(weatherData.temperature) }°</div>
-                <div className='description'>{ weatherData.description } </div>
-                <div className='feelsLike'>Feels like { Math.round(weatherData.feelsLike) }°</div>
-                <div className='minMax'>
-                  { Math.round(weatherData.min) }° | <strong>{ Math.round(weatherData.max) }°</strong>
+                }}
+              >
+                <div className="degree">
+                  {Math.round(weatherData.temperature)}°
+                </div>
+                <div className="description">{weatherData.description} </div>
+                <div className="feelsLike">
+                  Feels like {Math.round(weatherData.feelsLike)}°
+                </div>
+                <div className="minMax">
+                  {Math.round(weatherData.min)}° |{" "}
+                  <strong>{Math.round(weatherData.max)}°</strong>
                 </div>
 
-                <div className='positionTime'>
-                  <div className='position'>
-                    <a
-                      href='/'
-                      className='btn geoButton'
-                    >
-                      <i className='bi bi-geo-alt'></i>
+                <div className="positionTime">
+                  <div className="position">
+                    <a href="/" className="btn geoButton">
+                      <i className="bi bi-geo-alt"></i>
                     </a>
-                    <div className='city'>{ weatherData.city }, { weatherData.country }</div>
+                    <div className="city">
+                      {weatherData.city}, {weatherData.country}
+                    </div>
                   </div>
-                  <div className='time '><FormatedTimezone timezone={ weatherData.timezone } /></div>
+                  <div className="time ">
+                    <FormatedTimezone timezone={weatherData.timezone} />
+                  </div>
                 </div>
               </div>
             </div>
           </div>
         </div>
-        <div className=""><Infos data={ weatherData } /></div>
-      </div >
-    )
-
+        <div className="">
+          <Infos data={weatherData} />
+        </div>
+      </div>
+    );
   } else {
-    return "Loading..."
     search();
+    return "Loading...";
   }
-
 }
