96b534595a426b3dbfa310fb55cd168508513986
annema01 <amrobert.animation@gmail.com>
2023-09-05 13:12:49 -0400

    removed forcast copy - created instructions-todo file

--- src/App.js
+++ src/App.js
@@ -1,73 +1,34 @@
-import "./App.css"
-import Weather from "./Weather"
-import Forcast from "./Forcast"
-import Footer from "./Footer"
+import "./App.css";
+import Weather from "./Weather";
+import Forcast from "./Forcast";
+import Footer from "./Footer";
 
 function App() {
   return (
-    <div className='App g-0'>
+    <div className="App g-0">
       <div className="row g-0 contentAll">
-        <div className="col-md-7"><Weather defaultCity="Montreal" />
+        <div className="col-md-7">
+          <Weather defaultCity="Montreal" />
         </div>
         <div className=" col-md-5 g-0 forcastSection">
           <Forcast
-            weekday='Wednesday'
-            weekdayAbbr='Wed.'
-            icon='sun'
-            fill='yellow'
-            min='16'
-            max='25'
-            collapse='day1'
-            target='#day1'
-          />
-          <Forcast
-            weekday='Thursday'
-            weekdayAbbr='Thu.'
-            icon='cloud-rain'
-            fill='blue'
-            min='7'
-            max='17'
-            collapse='day2'
-          />
-          <Forcast
-            weekday='Friday'
-            weekdayAbbr='Fri.'
-            icon='cloud-sun'
-            fill='blue'
-            min='9'
-            max='19'
-            collapse='day3'
-          />
-          <Forcast
-            weekday='Saturday'
-            weekdayAbbr='Sat.'
-            icon='cloud-rain'
-            fill='blue'
-            min='11'
-            max='16'
-            collapse='day4'
-          />
-          <Forcast
-            weekday='Sunday'
-            weekdayAbbr='Sun.'
-            icon='sun'
-            fill='yellow'
-            min='9'
-            max='20'
-            collapse='day5'
+            weekday="Wednesday"
+            weekdayAbbr="Wed."
+            icon="sun"
+            fill="yellow"
+            min="16"
+            max="25"
+            collapse="day1"
+            target="#day1"
           />
         </div>
         <Footer />
       </div>
-      <a
-        href='#scrollUp'
-        className='btn scrollUp'
-      >
-        <i className='bi bi-caret-up-fill'></i>
+      <a href="#scrollUp" className="btn scrollUp">
+        <i className="bi bi-caret-up-fill"></i>
       </a>
-
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/Weather.js
+++ src/Weather.js
@@ -8,20 +8,18 @@ import axios from "axios";
 import UnitLinks from "./UnitLinks";
 import CurrentWeatherInfos from "./CurrentWeatherInfos";
 
-
 export default function Weather(props) {
-  const [ weatherData, setWeatherData ] = useState({ ready: false }); // ready used to be a state but it was put as another data from the WeatherData
-  const [ city, setCity ] = useState(props.defaultCity);
-  const [ unit, setUnit ] = useState("metric");
-
+  const [weatherData, setWeatherData] = useState({ ready: false }); // ready used to be a state but it was put as another data from the WeatherData
+  const [city, setCity] = useState(props.defaultCity);
+  const [unit, setUnit] = useState("metric");
 
   function handleResponse(response) {
     console.log(response.data);
-    let rain = response.data.rain && response.data.rain[ '1h' ];
+    let rain = response.data.rain && response.data.rain["1h"];
     if (rain === undefined) {
       rain = "-";
     }
-    let snow = response.data.snow && response.data.snow[ '1h' ];
+    let snow = response.data.snow && response.data.snow["1h"];
     if (snow === undefined) {
       snow = "-";
     }
@@ -35,8 +33,8 @@ export default function Weather(props) {
       max: response.data.main.temp_max,
       city: response.data.name,
       country: response.data.sys.country,
-      description: response.data.weather[ 0 ].description,
-      icon: response.data.weather[ 0 ].icon,
+      description: response.data.weather[0].description,
+      icon: response.data.weather[0].icon,
       date: new Date(response.data.dt * 1000),
       timezone: response.data.timezone,
       //infos
@@ -52,10 +50,8 @@ export default function Weather(props) {
       clouds: response.data.clouds.all,
       sunrise: new Date(response.data.sys.sunrise * 1000),
       sunset: new Date(response.data.sys.sunset * 1000),
-      longitude: response.data.coord.lon,
-      latitude: response.data.coord.lat,
+      coordinates: response.data.coord,
     });
-
   }
 
   function handleSubmit(event) {
@@ -72,14 +68,13 @@ export default function Weather(props) {
     axios.get(apiUrl).then(handleResponse);
   }
 
-
   useEffect(() => {
     //code i want to run
     search();
 
     // return function (optionnal)
     // eslint-disable-next-line
-  }, [ unit ]);//dependency array
+  }, [unit]); //dependency array
 
   function showFahrenheit(event) {
     event.preventDefault();
@@ -91,56 +86,55 @@ export default function Weather(props) {
     setUnit("metric");
   }
 
-
-
   if (weatherData.ready) {
     return (
       <div className="Weather">
         <div className="container content">
           <HeaderApp
-            submit={ handleSubmit }
-            cityChange={ handleCityChange }
-            celcius={ showCelcius }
-            fahrenheit={ showFahrenheit }
-            unit={ unit } />
-
+            submit={handleSubmit}
+            cityChange={handleCityChange}
+            celcius={showCelcius}
+            fahrenheit={showFahrenheit}
+            unit={unit}
+          />
 
           <div className="contentWeather">
             <div className="updateTime">
               Updated
               <span className="formatedTime">
-                <FormatedDate date={ weatherData.date } />
+                <FormatedDate date={weatherData.date} />
               </span>
             </div>
-            {/* Buttons top */ }
+            {/* Buttons top */}
 
             <a href="#forcast" className="btn forcastAnchor">
               5 days
               <i className="bi bi-caret-down"></i>
             </a>
             <UnitLinks
-              celciusFunction={ showCelcius }
-              fahrenheitFunction={ showFahrenheit }
-              unit={ unit }
-              mobile="Sm" />
+              celciusFunction={showCelcius}
+              fahrenheitFunction={showFahrenheit}
+              unit={unit}
+              mobile="Sm"
+            />
 
             <div>
               <CurrentWeatherInfos
-                icon={ weatherData.icon }
-                temperature={ weatherData.temperature }
-                description={ weatherData.description }
-                feelsLike={ weatherData.feelsLike }
-                min={ weatherData.min }
-                max={ weatherData.max }
-                city={ weatherData.city }
-                country={ weatherData.country }
-                timezone={ weatherData.timezone } />
-
+                icon={weatherData.icon}
+                temperature={weatherData.temperature}
+                description={weatherData.description}
+                feelsLike={weatherData.feelsLike}
+                min={weatherData.min}
+                max={weatherData.max}
+                city={weatherData.city}
+                country={weatherData.country}
+                timezone={weatherData.timezone}
+              />
             </div>
           </div>
         </div>
         <div>
-          <Infos data={ weatherData } />
+          <Infos data={weatherData} />
         </div>
       </div>
     );
