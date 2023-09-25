9d1db904eca42a16988941fdd4665c466ce13c62
annema01 <123336005+annema01@users.noreply.github.com>
2023-09-25 17:09:31 -0400

    deploy test

--- src/Containers/Forcast.js
+++ src/Containers/Forcast.js
@@ -9,9 +9,9 @@ import ForcastDay from "../Components/ForcastDay";
 //import { RotatingLines } from "react-loader-spinner";
 
 export default function Forcast(props) {
-  let [loaded, setLoaded] = useState(false);
-  let [forcast, setForcast] = useState(null);
-  let day = ["day1", "day2", "day3", "day4", "day5"];
+  let [ loaded, setLoaded ] = useState(false);
+  let [ forcast, setForcast ] = useState(null);
+  let day = [ "day1", "day2", "day3", "day4", "day5" ];
 
   function handleResponse(response) {
     // console.log(response.data.daily);
@@ -34,43 +34,33 @@ export default function Forcast(props) {
     //code i want to run
     setLoaded(false);
     // return function (optionnal)
-  }, [props.longitude]); //dependency array
+  }, [ props.longitude ]); //dependency array
 
   useEffect(() => {
     //code i want to run
     setLoaded(false);
     // return function (optionnal)
-  }, [props.unit]); //dependency array
+  }, [ props.unit ]); //dependency array
 
   if (props.longitude && props.latitude) {
     if (loaded) {
       return (
         <div>
-          {forcast.map(function (dailyForecast, index) {
+          { forcast.map(function (dailyForecast, index) {
             if (index < 5) {
               return (
-                <div key={index}>
-                  <ForcastDay data={dailyForecast} collapse={day[index]} />
+                <div key={ index }>
+                  <ForcastDay data={ dailyForecast } collapse={ day[ index ] } />
                 </div>
               );
             } else {
               return null;
             }
-          })}
+          }) }
         </div>
       );
     } else {
-      {
-        /* <div className="col d-flex justify-content-center pt-4">
-        <RotatingLines
-          strokeColor="#017eff"
-          strokeWidth="2"
-          animationDuration="1.5"
-          width="96"
-          visible={ true }
-        />
-      </div> */
-      }
+
       Load();
       return null;
     }
